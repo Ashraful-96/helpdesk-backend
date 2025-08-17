02183dd999d45e829beaef13f998c7d1dcc758f1
Ashraful-96 <ashraful.islam.0496@gmail.com>
2025-08-17 17:06:13 +0600

    add issue file entity and implementation

--- src/main/java/com/aust/its/controller/DeveloperController.java
+++ src/main/java/com/aust/its/controller/DeveloperController.java
@@ -6,7 +6,6 @@ import com.aust.its.service.DeveloperService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
---
+++ src/main/java/com/aust/its/entity/IssueFile.java
@@ -0,0 +1,29 @@
+package com.aust.its.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class IssueFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "file_name", nullable = false)
+    private String fileName;
+
+    @ManyToOne
+    @JoinColumn(name = "issue_id")
+    private Issue issue;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
